908c7e9ca72f399d8d76b0c5fef7bcb945af8ec8
naju <juanlugo@ciencias.unam.mx>
2019-09-09 23:07:49 -0500

    Manejo de archivos, verificar imagenes y establecer guardado de imagenes

--- build-InterfazGrafica-Desktop-Debug/moc_gui.cpp
+++ build-InterfazGrafica-Desktop-Debug/moc_gui.cpp
@@ -1,7 +1,7 @@
 /****************************************************************************
 ** Meta object code from reading C++ file 'gui.h'
 **
-** Created by: The Qt Meta Object Compiler version 67 (Qt 5.9.5)
+** Created by: The Qt Meta Object Compiler version 67 (Qt 5.12.2)
 **
 ** WARNING! All changes made in this file will be lost!
 *****************************************************************************/
@@ -12,7 +12,7 @@
 #if !defined(Q_MOC_OUTPUT_REVISION)
 #error "The header file 'gui.h' doesn't include <QObject>."
 #elif Q_MOC_OUTPUT_REVISION != 67
-#error "This file was generated using the moc from 5.9.5. It"
+#error "This file was generated using the moc from 5.12.2. It"
 #error "cannot be used with the include files from this version of Qt."
 #error "(The moc has changed too much.)"
 #endif
@@ -57,7 +57,7 @@ QT_MOC_LITERAL(10, 221, 3) // "key"
 static const uint qt_meta_data_GUI[] = {
 
  // content:
-       7,       // revision
+       8,       // revision
        0,       // classname
        0,    0, // classinfo
        7,   14, // methods
@@ -91,7 +91,7 @@ static const uint qt_meta_data_GUI[] = {
 void GUI::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, void **_a)
 {
     if (_c == QMetaObject::InvokeMetaMethod) {
-        GUI *_t = static_cast<GUI *>(_o);
+        auto *_t = static_cast<GUI *>(_o);
         Q_UNUSED(_t)
         switch (_id) {
         case 0: _t->on_actionNuev_Imagen_triggered(); break;
@@ -106,10 +106,14 @@ void GUI::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, void **
     }
 }
 
-const QMetaObject GUI::staticMetaObject = {
-    { &QMainWindow::staticMetaObject, qt_meta_stringdata_GUI.data,
-      qt_meta_data_GUI,  qt_static_metacall, nullptr, nullptr}
-};
+QT_INIT_METAOBJECT const QMetaObject GUI::staticMetaObject = { {
+    &QMainWindow::staticMetaObject,
+    qt_meta_stringdata_GUI.data,
+    qt_meta_data_GUI,
+    qt_static_metacall,
+    nullptr,
+    nullptr
+} };
 
 
 const QMetaObject *GUI::metaObject() const
